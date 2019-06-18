f202d932863f7cf82867000e4e463d2ec76a3923
Noah Teshima <noah.teshima@gmail.com>
2019-06-18 12:35:15 -0700

    Fixed foiling of complex elements in dft matrix

--- src/dft.js
+++ src/dft.js
@@ -4,10 +4,17 @@ This is done using a brute-force O(N^2) algorithm on the complex
 plane.
 */
 
+const Complex = require('npm-complex').Complex;
+
 /**
 brute-force O(N^2) calculation for the DFT,
 where N represents the number of samples in
-a time-based domain
+a time-based domain. The collection of samples
+is not limited to the field of reals. Instead,
+our time-based domain may also encapsulate complex
+numbers in the form [re, im].
+
+@param {number[][]} samp
 */
 let dft = function(samp) {
   // guard clause to keep valid samples
@@ -18,29 +25,29 @@ let dft = function(samp) {
   // from DFT
   let F = [],
   N = samp.length,
+  compSum = new Complex(),
+  compOne = new Complex(),
+  compTwo = new Complex(),
   // complex element in the form [re, im]. this algorithm
   // uses Euler's instead to compute the real and imaginary
   // components
-  re, im,
   theta;
   // number of given samples will be the number of frequency bins
   for(let n = 0; n < N; n++) {
-    re = im = 0;
+    compSum.setTuple([0, 0]);
     for(let k = 0; k < N; k++) {
+      compOne.setTuple([samp[k][0], samp[k][1]]);
       theta = (2 * Math.PI * n * k) / N;
-      // real correlation coefficient
-      re = (samp[k] * Math.cos(theta));
-      // imaginary component
-      im -= (samp[k] * Math.sin(theta));
+      compTwo.setTuple([Math.cos(theta), -Math.sin(theta)]);
+      compOne.foil(compTwo.getTuple());
+      compSum.addTuple(compOne.getTuple());
     }
-    F[n] = [re, im];
+    F[n] = compSum.getTuple();
   }
   return F;
 }
 
-module.exports = {
-  dft: dft
-}
+module.exports = dft;
 
 
 
